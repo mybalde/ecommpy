5bb152969f0000e08640e00d28309d9f88ab945f
mybalde <balde@MacBook-Pro-de-Balde.local>
2022-02-06 17:52:01 +0100

    Create Cart & Cart Item Models

--- carts/admin.py
+++ carts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from carts.models import Cart, CartItem
+
+admin.site.register(Cart)
+admin.site.register(CartItem)
--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from store.models import Product
+
+class Cart(models.Model):
+    cart_id = models.CharField(max_length=250, blank=True)
+    date_added = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.cart_id
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.product
