6af1e31659851d18c429b4b01a61735252ee0ed5
Mamadou Yéro Baldé <balde@MacBook-Pro-de-Balde.local>
2022-02-06 01:29:55 +0100

    Make Context Processors for Displaying Categories on Navbar

--- store/views.py
+++ store/views.py
@@ -2,18 +2,16 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
 
+
 def store(req, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.all().filter(category=category, is_available=True)
         products_count = products.count()
-        categories = Category.objects.all().order_by('category_name')
     else:
         products = Product.objects.all().filter(is_available=True)
         products_count = products.count()
-        categories = Category.objects.all().order_by('category_name')
 
-    context = {'products': products,
-               'products_count': products_count, 'categories': categories}
-    print(categories)
+    context = {'products': products, 'products_count': products_count}
+
     return render(req, 'store/store.html', context)
