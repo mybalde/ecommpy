af1766a0b5f3ae9257d16ecbd769cbb734972012
mybalde <balde@MacBook-Pro-de-Balde.local>
2022-02-12 21:43:12 +0100

    increment, decrement, and remove product

--- carts/urls.py
+++ carts/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns=[
     path('', views.cart, name='cart'),
-    path('add_cart/<str:produit_id>', views.add_cart, name='add_cart'),
+    path('add_cart/<str:product_id>', views.add_cart, name='add_cart'),
+    path('remove_cart/<str:product_id>', views.remove_cart, name='remove_cart'),
+    path('remove/<str:product_id>', views.remove, name='remove'),
 ]
--- carts/views.py
+++ carts/views.py
@@ -1,20 +1,29 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from store.models import Product
 from .models import Cart,CartItem
-
-def cart(req):
-    return render(req, 'store/carts.html')
 
 def _get_cart_id(req):
     '''get cart id from request session'''
     return id if (id := req.session.session_key) else req.session.create()
     
-        
+def cart(req):
+    context = {'quantity': 0, 'total': 0 }
+    try:
+        cart = Cart.objects.get(cart_id = _get_cart_id(req))
+        cart_items = CartItem.objects.filter(cart=cart)
+        context['cart_items'] = cart_items
+        for cart_item in cart_items:
+            context['total'] += cart_item.sub_total()
+            context['quantity'] += cart_item.quantity
+    except: 
+        pass
 
-def add_cart(req, produit_id):
+    return render(req, 'store/carts.html', context) 
+
+def add_cart(req, product_id):
     '''add a product in the cart'''
-    product = Product.objects.get(product_name=produit_id)
+    product = Product.objects.get(product_name=product_id)
     try:
         cart = Cart.objects.get(cart_id = _get_cart_id(req))
     except Cart.DoesNotExist:
@@ -27,4 +36,24 @@ def add_cart(req, produit_id):
     except CartItem.DoesNotExist: 
         cart_item = CartItem.objects.create(product=product, cart=cart, quantity=1)
     cart_item.save()
+    return redirect('cart')
+
+def remove_cart(req, product_id):
+    cart = Cart.objects.get(cart_id=_get_cart_id(req))
+    product = get_object_or_404(Product, product_name=product_id)
+    cart_item = CartItem.objects.get(cart=cart, product=product)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    
+    return redirect('cart')
+
+def remove(req, product_id):
+    cart = Cart.objects.get(cart_id=_get_cart_id(req))
+    product = get_object_or_404(Product, product_name=product_id)
+    cart_item = CartItem.objects.get(cart=cart, product=product)
+    cart_item.delete()
+    
     return redirect('cart')
